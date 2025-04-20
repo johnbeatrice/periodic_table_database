89fa645b4c09e619d14de9330aa6d8cc01f7b7b0
John Beatrice <johncbeatrice@gmail.com>
2025-04-20 18:22:49 +0000

    feat: first planning stage of element.sh

--- element.sh
+++ element.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
+# read me
 # This script take input related to elements and outputs details about those elements.
 # If you run ./element.sh 1, ./element.sh H, or ./element.sh Hydrogen, 
-# it should output only "The element with atomic number 1 is Hydrogen (H). 
-# It's a nonmetal, with a mass of 1.008 amu. 
-# Hydrogen has a melting point of -259.1 celsius and a boiling point of -252.9 celsius.""
+# it should output only "The element with atomic number 1 is Hydrogen (H). It's a nonmetal, with a mass of 1.008 amu. Hydrogen has a melting point of -259.1 celsius and a boiling point of -252.9 celsius.""
 
 # echo "git branch -m main" (run this in terminal while on "master" branch to change its name)
 
 echo "Please provide an element as an argument."
+
+
+# how to get script to take user arguments (./element.sh H)
+# element="$($PSQL "SELECT * FROM elements WHERE column = $user_input")"
+# properties="$($PSQL "ELECT * FROM properties WHERE column = $user_input")"
+
+# need if statements to check if user input is integer, 1 or 2 letters (symbol) or more than 2 letters (name)
+# then use the appropriate sql queries (searching by user input, there will be 3 options)
+# then return the appropriate response with requested details
